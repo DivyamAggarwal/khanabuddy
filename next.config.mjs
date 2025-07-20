d4ae2e43b6c1c6223bdc45331ec1125df6e79cbd
DivyamAggarwal <mailtodivyam1@gmail.com>
2025-07-20 19:34:12 +0530

    changes some

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,18 @@ const nextConfig = {
     domains: ["images.unsplash.com"],
   },
   experimental: {
-    useLightningcss: false
-  }
+    useLightningcss: false,
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      // Force disable lightningcss
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'lightningcss': false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
